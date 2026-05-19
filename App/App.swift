ed5ce0ee500822d1d0136b4455895709c996bdb7
jmccloud827 <60444521+jmccloud827@users.noreply.github.com>
2026-05-19 15:53:16 -0400

    Add icon

--- App/App.swift
+++ App/App.swift
@@ -5,7 +5,16 @@ import SwiftData
 struct App: SwiftUI.App {
     var body: some Scene {
         WindowGroup {
-            GamesList()
+            Image(systemName: "suit.spade.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(.white)
+                    .frame(width: 300, height: 300)
+                .padding()
+                .padding()
+                .padding()
+                .padding()
+                .background(.accent.gradient)
         }
         .modelContainer(for: Game.self)
     }
@@ -13,14 +22,13 @@ struct App: SwiftUI.App {
 
 /// App Icon
 #Preview {
-    Image(systemName: "document.viewfinder")
+    Image(systemName: "suit.spade.fill")
         .resizable()
         .aspectRatio(contentMode: .fit)
         .foregroundStyle(.white)
-            .frame(width: 300, height: 300)
+            .frame(width: 250, height: 250)
         .padding()
         .padding()
-        .padding()
-        .padding()
-        .background(Color("Color").gradient)
+        .padding(44)
+        .background(.accent.gradient)
 }
